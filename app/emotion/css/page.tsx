81cb6b32438e9f2625ce52f58db239525dc436c0
Kidongg <dlrlehd7@daum.net>
2023-11-25 13:37:20 +0900

    emotion css

---
+++ app/emotion/css/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { css } from "@emotion/css";
+import React from "react";
+
+const page = () => {
+  return (
+    <div
+      className={css`
+        background-color: blueviolet;
+      `}
+    >
+      EMOTION-CSS
+    </div>
+  );
+};
+
+export default page;
