298b6d911ee8f367b1a274a058dacd09323626c8
Kidongg <dlrlehd7@daum.net>
2023-11-25 13:37:34 +0900

    emotion styled

---
+++ app/emotion/styled/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import styled from "@emotion/styled";
+import React from "react";
+
+const page = () => {
+  return <Nav>EMOTION-STYLED</Nav>;
+};
+
+const Nav = styled.div`
+  background-color: blueviolet;
+`;
+
+export default page;
