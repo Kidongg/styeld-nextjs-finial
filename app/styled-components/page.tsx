6d80be0d68a236afe182c12cd3e6a70fccb2281a
Kidongg <dlrlehd7@daum.net>
2023-11-25 13:37:07 +0900

    styled-components

---
+++ app/styled-components/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+
+const page = () => {
+  return <Nav>STYLED-COMPONENTS</Nav>;
+};
+
+const Nav = styled.div`
+  background-color: blueviolet;
+`;
+
+export default page;
